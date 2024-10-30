a11a72c8385e746ed90488dcb87cb0e4273c79bf
Nhơn Trần <nhonvn2509@gmail.com>
2024-10-30 20:21:45 +0700

    Add vite-plugin-singlefile and configure Rollup input in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/react-table-pagination/',
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
 })
